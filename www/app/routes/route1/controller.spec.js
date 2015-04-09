5a27b536a20c05221520024f19a202a518e6dbac
Aleksandar Djindjic <djindjic@gmail.com>
2015-04-09 23:37:36 +0200

    resource links

--- www/app/routes/route1/controller.spec.js
+++ www/app/routes/route1/controller.spec.js
@@ -1,3 +1,11 @@
+// https://github.com/sydcanem/angularjs-testing-cheat-sheet
+// http://stackoverflow.com/questions/20433485/angular-ui-router-unit-testing-states-to-urls
+// http://www.jvandemo.com/how-to-resolve-angularjs-resources-with-ui-router/
+// http://stackoverflow.com/questions/28557749/angular-jasmine-ui-router-inject-resolve-value-into-test
+// http://bardo.io/posts/testing-your-ui-router-configuration/
+// http://stackoverflow.com/questions/22569574/angularjs-unit-test-a-controller-dependent-on-route-resolve
+
+
 // import angular from 'angular';
 // import mocks from 'angular-mocks';
 // import echoServiceModule from 'app/services/echo.service';
