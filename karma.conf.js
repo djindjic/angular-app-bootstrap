29dfdb2b9160135d517d626cd90d45da60957746
Aleksandar Djindjic <djindjic@gmail.com>
2015-06-03 16:49:35 +0200

    karma config
    Signed-off-by: Aleksandar Djindjic <djindjic@gmail.com>

--- karma.conf.js
+++ karma.conf.js
@@ -1,15 +1,8 @@
 module.exports = function(config) {
   var configuration = {
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '',
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['jspm', 'mocha', 'chai'],
 
     jspm: {
-      // Edit this to your needs
       loadFiles: ['www/app/**/*.spec.js'],
       serveFiles: ['www/app/**/*.js', 'www/app/**/*.html']
     },
@@ -18,50 +11,13 @@ module.exports = function(config) {
       '/base/': '/base/www/'
     },
 
-    // list of files / patterns to load in the browser
-    files: [
-    ],
-
-
-    // list of files to exclude
-    exclude: [
-    ],
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-    },
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-
-//    reporters: ['progress'],
-
     reporters: ['mocha'],
 
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-    // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
     browsers: ['Chrome'],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
     singleRun: false
   };
 
